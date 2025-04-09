ebc1fad5ab08d6a7e6ab1070e7290909cb0369fe
kavya478 <kummithakavya@gmail.com>
2025-04-09 07:13:52 +0530

    add

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+
+#Author: kavya
+# date: 09-04-2025
+
+movies=("kavya","Divya", "surya")
+
+echo print first movie {[0]}
+echo print second movie {[1]}
+echo print third  movie {[2]}
