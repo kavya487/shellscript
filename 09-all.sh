9151adc268e10661cc3f975a846ff762b6f7c26a
kavya478 <kummithakavya@gmail.com>
2025-04-10 06:30:53 +0530

    add

--- 09-all.sh
+++ 09-all.sh
@@ -9,16 +9,20 @@
 # echo  please enter the password
 #  read -s   password
 # echo password is give
-
 # mylist=(12  "hello" "kavs" "as" "hgf" 132)
-
 # echo array is ${mylist[0]} ${mylist[2]}
 # echo  all values  in array is ${mylist[*]}
 # echo   no of values  in array is ${#mylist[*]}
 # echo  values from index in  array is ${mylist[*]:2:5}
-array="hello world"
-echo length=${#array}
-echo capital values is upper=${array^^}
- echo lower value is lower =${array,,} 
-echo "${array/world/kavbya}"
-echo "${array:6:5}"
+# array="hello world"
+# echo length=${#array}
+# echo capital values is upper=${array^^}
+#  echo lower value is lower =${array,,} 
+# echo "${array/world/kavbya}"
+# echo "${array:6:5}"
+
+
+x=20
+y=12
+sum=(x+y)
+echo sum of two numbers is  $sum
