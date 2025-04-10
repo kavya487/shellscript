547b348b2654b6d226acaceacba9836396218c3c
kavya478 <kummithakavya@gmail.com>
2025-04-10 06:03:54 +0530

    add

--- 09-all.sh
+++ 09-all.sh
@@ -3,12 +3,13 @@
 # kavya=good-morning
 
 # echo "Hi $kavya"
+# echo  please enter the hostname 
+# read hostname
+# echo hostname was provided $hostname 
+# echo  please enter the password
+#  read -s   password
+# echo password is give
 
+mylist={1,2,hello,kavs}
 
- 
-echo  please enter the hostname 
-read hostname
-echo hostname was provided $hostname 
-echo  please enter the password
- read -s   password
-echo password is given 
+echo array is $mylist([0],[3])
